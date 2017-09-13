a85ffc1736a5de90aa6e32c8bfeb55d0bda2c828
luonglearnstocode <luong.nguyen@metropolia.fi>
2017-09-13 21:18:19 +0300

    hw3 part 1 - reading the corpus as an input file

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-# PennX: SD2x Data Structures and Software Design
-https://www.edx.org/course/data-structures-software-design-pennx-sd2x
-* [Homework 1 - Linked Lists](https://courses.edx.org/courses/course-v1:PennX+SD2x+2T2017/courseware/44fa2fc239fa479baabfb7cbac8bcfb6/d16178c5ecce4daab963b6f46389992a/?activate_block_id=block-v1%3APennX%2BSD2x%2B2T2017%2Btype%40sequential%2Bblock%40d16178c5ecce4daab963b6f46389992a)
+# [PennX: SD2x Data Structures and Software Design](https://www.edx.org/course/data-structures-software-design-pennx-sd2x)
+* [Homework 1 - Linked Lists](https://courses.edx.org/courses/course-v1:PennX+SD2x+2T2017/courseware/44fa2fc239fa479baabfb7cbac8bcfb6/d16178c5ecce4daab963b6f46389992a/?activate_block_id=block-v1%3APennX%2BSD2x%2B2T2017%2Btype%40sequential%2Bblock%40d16178c5ecce4daab963b6f46389992a)  
 Implement three methods that perform functions on a linked list, using the java.util.LinkedList class from the Java Collections API.
-* [Homework 2 - Queues and Stacks](https://courses.edx.org/courses/course-v1:PennX+SD2x+2T2017/courseware/44fa2fc239fa479baabfb7cbac8bcfb6/c246839fafee4770975035fd1c2bed51/?activate_block_id=block-v1%3APennX%2BSD2x%2B2T2017%2Btype%40sequential%2Bblock%40c246839fafee4770975035fd1c2bed51)
+* [Homework 2 - Queues and Stacks](https://courses.edx.org/courses/course-v1:PennX+SD2x+2T2017/courseware/44fa2fc239fa479baabfb7cbac8bcfb6/c246839fafee4770975035fd1c2bed51/?activate_block_id=block-v1%3APennX%2BSD2x%2B2T2017%2Btype%40sequential%2Bblock%40c246839fafee4770975035fd1c2bed51)  
 Use the stack and queue implementations from the Java Collections API in order to build a program that determines whether an HTML page is well formatted.
-* [Homework 3 - Collections](https://courses.edx.org/courses/course-v1:PennX+SD2x+2T2017/courseware/44fa2fc239fa479baabfb7cbac8bcfb6/486c0e96406d4cd898628ed07612cb67/?child=first)
+* [Homework 3 - Collections](https://courses.edx.org/courses/course-v1:PennX+SD2x+2T2017/courseware/44fa2fc239fa479baabfb7cbac8bcfb6/486c0e96406d4cd898628ed07612cb67/?child=first)  
 Write a program that will analyze the sentiment (positive or negative) of a sentence based on the words it contains by implementing methods that use the List, Set, and Map interfaces from the Java Collections API.
--- SD2x-HW3/src/Analyzer.java
+++ SD2x-HW3/src/Analyzer.java
@@ -1,7 +1,12 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /*
  * SD2x Homework #3
@@ -11,14 +16,39 @@ import java.util.Set;
 public class Analyzer {
 	
 	/*
-	 * Implement this method in Part 1
+	 * Takes the name of the file to read and read it one line at a time, creating Sentence objects and putting them into the List. 
+	 * Returns a List containing Sentence objects.
 	 */
 	public static List<Sentence> readFile(String filename) {
-
-		/* IMPLEMENT THIS METHOD! */
+		List<Sentence> sentences = new ArrayList<>();
+		List<String> lines = new ArrayList<>();
 		
-		return null; // this line is here only so this code will compile if you don't modify it
-
+		/*
+		 * https://stackoverflow.com/questions/4716503/reading-a-plain-text-file-in-java
+		 */
+		try {
+			lines = Files.lines(Paths.get(filename)).collect(Collectors.toList());
+		} catch (Exception e) {
+			return new ArrayList<Sentence>(); // if the file cannot be opened for reading or if the input filename is null, this method should return an empty List.
+		}
+		
+		for (String line : lines) {
+			int spaceIndex = line.indexOf(" ");
+			 
+			try { 
+				int score = Integer.parseInt(line.substring(0, spaceIndex));
+				String text = line.substring(spaceIndex + 1); 
+				
+				if (score <= 2 && score >= -2 && !text.isEmpty()) { // well formatted
+					sentences.add(new Sentence(score, text));
+				}
+			} catch(Exception e) {
+				continue;
+			}
+			
+		}
+		
+		return sentences; 
 	}
 	
 	/*
@@ -59,19 +89,25 @@ public class Analyzer {
 	 * You may modify it as needed.
 	 */
 	public static void main(String[] args) {
-		if (args.length == 0) {
-			System.out.println("Please specify the name of the input file");
-			System.exit(0);
-		}
-		String filename = args[0];
-		System.out.print("Please enter a sentence: ");
-		Scanner in = new Scanner(System.in);
-		String sentence = in.nextLine();
-		in.close();
+//		if (args.length == 0) {
+//			System.out.println("Please specify the name of the input file");
+//			System.exit(0);
+//		}
+//		String filename = args[0];
+//		System.out.print("Please enter a sentence: ");
+//		Scanner in = new Scanner(System.in);
+//		String sentence = in.nextLine();
+//		in.close();
+//		List<Sentence> sentences = Analyzer.readFile(filename);
+//		Set<Word> words = Analyzer.allWords(sentences);
+//		Map<String, Double> wordScores = Analyzer.calculateScores(words);
+//		double score = Analyzer.calculateSentenceScore(wordScores, sentence);
+//		System.out.println("The sentiment score is " + score);
+		
+		String filename = "reviews.txt";
 		List<Sentence> sentences = Analyzer.readFile(filename);
-		Set<Word> words = Analyzer.allWords(sentences);
-		Map<String, Double> wordScores = Analyzer.calculateScores(words);
-		double score = Analyzer.calculateSentenceScore(wordScores, sentence);
-		System.out.println("The sentiment score is " + score);
+		for (Sentence sentence : sentences) {
+			System.out.println(sentence.getScore() + " " + sentence.getText());
+		}
 	}
 }
