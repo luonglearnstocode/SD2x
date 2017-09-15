dc6d874656c630b1c92f08143119e370742d1883
luonglearnstocode <luong.nguyen@metropolia.fi>
2017-09-15 05:31:34 +0300

    HW5 passed all tests

--- HW5/src/MovieRatingsProcessor.java
+++ HW5/src/MovieRatingsProcessor.java
@@ -5,7 +5,9 @@
  */
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.TreeMap;
 
@@ -49,10 +51,50 @@ public class MovieRatingsProcessor {
 		return movies;
 	}
 	
+	/*
+	 * given an input int rating, modify the TreeMap object that was passed as an argument 
+	 * so that you remove all ratings in the PriorityQueue that are below 
+	 * (but not equal to) rating for every movie in the Map. 
+	 * 
+	 * If all ratings are removed from a movie’s PriorityQueue, then remove the mapping from the TreeMap. 
+	 * 
+	 * Additionally, this method should return a new TreeMap that maps a movie title 
+	 * to the number of ratings that were removed from its corresponding PriorityQueue.
+	 * The TreeMap that is returned should only contain movies that had ratings removed from its PriorityQueue.
+	 */
 	public static TreeMap<String, Integer> removeAllRatingsBelow(TreeMap<String, PriorityQueue<Integer>> movieRatings, int rating) {
+		if (movieRatings == null || movieRatings.isEmpty()) {
+			return new TreeMap<String, Integer>();
+		}
 		
-		/* IMPLEMENT THIS METHOD! */
+		TreeMap<String, Integer> result = new TreeMap<>();
 		
-		return null; // this line is here only so this code will compile if you don't modify it
+		/*
+		 * use Iterator to iterate the Map and the PriorityQueue of ratings 
+		 * in order to avoid ConcurrentModificationException
+		 */
+		Iterator<Map.Entry<String, PriorityQueue<Integer>>> entries = movieRatings.entrySet().iterator();
+		while (entries.hasNext()) {
+			Map.Entry<String, PriorityQueue<Integer>> entry = entries.next();
+			
+			Iterator<Integer> ratingsIT = entry.getValue().iterator();
+			while (ratingsIT.hasNext()) {
+				if (ratingsIT.next() < rating) {
+		    		ratingsIT.remove();
+		    		
+		    		if (!result.containsKey(entry.getKey())) { // title already in the result map
+		    			result.put(entry.getKey(), 1);
+		    		} else {
+		    			result.put(entry.getKey(), result.get(entry.getKey()) + 1);
+		    		}
+		    	}
+			}
+		    
+		    if (entry.getValue().isEmpty()) { // all ratings are removed
+		    	entries.remove();
+		    }
+		}
+
+		return result;
 	}
 }
