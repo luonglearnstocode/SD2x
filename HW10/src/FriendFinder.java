b4375125f2532bcb7ae5448db2df5d2c72253c71
luonglearnstocode <luong.nguyen@metropolia.fi>
2017-09-21 20:00:12 +0300

    HW10 all tests passed

--- HW10/src/FriendFinder.java
+++ HW10/src/FriendFinder.java
@@ -21,28 +21,54 @@ public class FriendFinder {
 	
 	
 	public Set<String> findClassmates(Student theStudent) {
+		if (classesDataSource == null || studentsDataSource == null) {
+			throw new IllegalStateException();
+		}
 		
-		String name = theStudent.getName();
+		if (theStudent == null) {
+			throw new IllegalArgumentException();
+		}
+		
+		String name = theStudent.getName();		
+		if (name == null) {
+			throw new IllegalArgumentException();
+		}
 		
 		// find the classes that this student is taking
-		List<String> myClasses = classesDataSource.getClasses(name);
+		List<String> myClasses = classesDataSource.getClasses(name);		
+		if (myClasses == null || myClasses.isEmpty()) {
+			return null;
+		}
 		
 		Set<String> classmates = new HashSet<String>();
 		
 		// use the classes to find the names of the students
 		for (String myClass : myClasses) {
 			// list all the students in the class
+			if (myClass == null) {
+				continue;
+			}
+			
 			List<Student> students = studentsDataSource.getStudents(myClass);
+			if (students == null || students.isEmpty()) {
+				return null;
+			}
 			
 			for (Student student : students) {
-				
+				if (student == null || student.getName() == null) {
+					continue;
+				}
 				// find the other classes that they're taking
 				List<String> theirClasses = classesDataSource.getClasses(student.getName());
-			
+				if (theirClasses == null) {
+					continue;
+				}
 				// see if all of the classes that they're taking are the same as the ones this student is taking
 				boolean same = true;
 				for (String c : myClasses) {
-					
+					if (c == null || c.length() == 0) {
+						continue;
+					}
 					if (theirClasses.contains(c) == false) {
 						same = false;
 						break;
